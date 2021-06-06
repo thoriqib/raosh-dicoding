3d2e5e494e3885f053c81ebabc246fb56bfe9e20
Thoriq Ibadurrohman <221810626@stis.ac.id>
2021-06-06 20:55:12 +0700

    app drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,41 +1,55 @@
-import "regenerator-runtime"; /* for async await transpile */
-import "../styles/main.scss";
-import * as json from "../DATA.json";
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.scss';
+import '../styles/responsive.scss';
+import * as json from '../DATA.json';
+import App from './views/app';
 
-const menuToggle = document.querySelector("#menu-toggle");
-const nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
 });
 
-const data = json.default.restaurants;
-console.log(data);
+const menuToggle = document.querySelector('#menu-toggle');
+const nav = document.querySelector('nav ul');
+const restaurantData = json.default.restaurants;
+const cardList = document.querySelector('.cards');
 
-const cardList = document.querySelector(".cards");
-let cards = "";
-data.forEach((el) => {
-  let star = "";
-  for (let i = 0; i < Math.floor(el.rating); i++) {
-    star += `<i class="bi bi-star-fill"></i>`;
+const getStar = (element) => {
+  let star = '';
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < Math.floor(element.rating); i++) {
+    star += '<i class="bi bi-star-fill"></i>';
   }
+  return star;
+};
 
-  cards += `<li class="cards_item">
+const getCard = (data) => {
+  let cards = '';
+  data.forEach((element) => {
+    const star = getStar(element);
+    cards += `<li class="cards_item">
     <div class="card">
       <div class="card_image">
-        <img src=${el.pictureId} alt=${el.name}/>
+        <img src=${element.pictureId} alt=${element.name}/>
       </div>
       <div class="card_content">
         <div class="card_label">
-          <div class="card_rating">${star}<span style="color: white;">${el.rating}</span></div>
-          <div class="card_city">${el.city}</div>
+          <div class="card_rating">${star}<span style="color: white;">${element.rating}</span></div>
+          <div class="card_city">${element.city}</div>
         </div>
-        <h2 class="card_title">${el.name}</h2>
-        <p class="card_text">${el.description}</p>
+        <h2 class="card_title">${element.name}</h2>
+        <p class="card_text">${element.description}</p>
         <button class="btn card_btn">Read More</button>
       </div>
     </div>
   </li>`;
+  });
+  return cards;
+};
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('slide');
 });
 
-cardList.innerHTML = cards;
+cardList.innerHTML = getCard(restaurantData);
--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,13 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/scripts/index.js"),
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -14,13 +15,13 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
           },
         ],
       },
@@ -28,7 +29,7 @@ module.exports = {
         test: /\.(png|svg|jpeg|jpg|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
@@ -36,14 +37,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/templates/index.html"),
-      filename: "index.html",
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/public/"),
-          to: path.resolve(__dirname, "dist/"),
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
         },
       ],
     }),
